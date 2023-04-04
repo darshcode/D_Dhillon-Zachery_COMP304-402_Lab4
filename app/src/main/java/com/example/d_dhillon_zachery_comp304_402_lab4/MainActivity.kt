401b990ebe368ce9202d3e8b797b954e728a5572
zachjhavers <zachjhavers@gmail.com>
2023-04-04 14:57:50 -0400

    Added Sign Up Page And Main Page, Got Sign Up Button On Main Page Working

--- app/src/main/java/com/example/d_dhillon_zachery_comp304_402_lab4/MainActivity.kt
+++ app/src/main/java/com/example/d_dhillon_zachery_comp304_402_lab4/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.d_dhillon_zachery_comp304_402_lab4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+
+        val signUpPageButton: Button = findViewById(R.id.signUpPageButton)
+
+        signUpPageButton.setOnClickListener{
+            val intent = Intent(this, StudentSignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
